3058dd8360ed55417c4c981c4072275738903ebf
Egor Ponomarev <egorponomarev.petruchcho@gmail.com>
2014-11-17 21:45:03 +0400

    add button in list is implemented

--- ExamsAndroid/src/com/petruchcho/examsandroid/ExamsListFragment.java
+++ ExamsAndroid/src/com/petruchcho/examsandroid/ExamsListFragment.java
@@ -2,15 +2,21 @@ package com.petruchcho.examsandroid;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.examsandroid.R;
+import com.petruchcho.examsandroid.ExamSettingsFragment.SettingsType;
 import com.petruchcho.examsandroid.exams.Exam;
 import com.petruchcho.examsandroid.exams.ExamsLab;
 
@@ -22,11 +28,34 @@ public class ExamsListFragment extends ListFragment {
 		super.onCreate(savedInstanceState);
 		exams = ExamsLab.getInstance(getActivity()).getExams();
 		getActivity().setTitle(R.string.exams_list_title);
-
 		Collections.sort(exams);
-
-		ExamAdapter adapter = new ExamAdapter(exams); 
+		setHasOptionsMenu(true);
+		ExamAdapter adapter = new ExamAdapter(exams);
 		setListAdapter(adapter);
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.f_exam_list, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_item_new_exam: {
+			Intent intent = new Intent(getActivity(),
+					ExamSettingsActivity.class);
+			intent.putExtra(ExamSettingsFragment.EXTRA_EXAM_ID,
+					UUID.randomUUID());
+			intent.putExtra(ExamSettingsFragment.EXTRA_SETTINGS_TYPE,
+					SettingsType.ADD);
+			startActivity(intent);
+			return true;
+		}
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	private class ExamAdapter extends ArrayAdapter<Exam> {
@@ -55,7 +84,7 @@ public class ExamsListFragment extends ListFragment {
 				daysLeftLabel.setVisibility(View.GONE);
 
 			nameLabel.setText(exam.getName());
-			countLabel.setText(""+exam.getCountOfQuestions());
+			countLabel.setText("" + exam.getCountOfQuestions());
 
 			return convertView;
 		}
